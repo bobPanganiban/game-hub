c8407cc8bfd4282d4ed0c02de304d3d25957ba71
Czar Panganiban <cpangnaiban@easyequities.com.ph>
2023-08-11 01:44:01 +0800

    filter games by genre

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,17 @@ import { Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
+import { useState } from "react";
+import { Genre } from "./hooks/useGenres";
 
 function App() {
+  const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
+
+  const handleOnSelectGenre = (genre: Genre) => {
+    setSelectedGenre(genre);
+    console.log(genre);
+  };
+
   return (
     <Grid
       templateAreas={{
@@ -12,7 +21,7 @@ function App() {
       }}
       templateColumns={{
         base: "1fr",
-        lg: "200px 1fr",
+        lg: "250px 1fr",
       }}
     >
       <GridItem area="nav">
@@ -20,11 +29,11 @@ function App() {
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" paddingX={5}>
-          <GenreList />
+          <GenreList onSelectGenre={handleOnSelectGenre} />
         </GridItem>
       </Show>
       <GridItem area="main">
-        <GameGrid />
+        <GameGrid selectedGenre={selectedGenre} />
       </GridItem>
     </Grid>
   );
--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,10 +1,9 @@
 const getCroppedImageUrl = (url: string) => {
-    console.log(url)
-    const target = 'media/';
-    const index = url.indexOf(target) + target.length;
-    const newUrl = url.slice(0, index) + 'crop/600/400/' + url.slice(index);
+  const target = "media/";
+  const index = url.indexOf(target) + target.length;
+  const newUrl = url.slice(0, index) + "crop/600/400/" + url.slice(index);
 
-    return newUrl;
-}
+  return newUrl;
+};
 
 export default getCroppedImageUrl;
