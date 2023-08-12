cf65f997589c88ef78bf4b808bf4d2d53d0b853e
Czar Panganiban <cpangnaiban@easyequities.com.ph>
2023-08-13 01:49:41 +0800

    refactor with react query

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,13 @@
 import axios from "axios";
 
+export interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
+
 export default axios.create({
-    baseURL: 'https://api.rawg.io/api',
-    params: {
-        key: 'fa8d8539792c40a1a74da4c329139b15'
-    }
-})
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "fa8d8539792c40a1a74da4c329139b15",
+  },
+});
